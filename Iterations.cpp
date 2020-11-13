f822fae54d4ead2ae634280b37d0859c7e578acb
alkornev <al.a.kornev@gmail.com>
2020-11-13 14:59:57 +0300

    Refactoring and some fixes

--- Iterations.cpp
+++ Iterations.cpp
@@ -2,16 +2,21 @@
 #include <cstdlib>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
 #include <omp.h>
 
 
 double** fillMatrix(int dim_A1, int dim_A2){
   double** res;
   res = (double**)malloc(dim_A1*sizeof(double*));
+
   for(int i = 0; i < dim_A1; i++){
     res[i] = (double*)malloc(dim_A2*sizeof(double));
+  }
+
+  for(int i = 0; i < dim_A1; i++){
     for(int j = 0; j < dim_A2; j++){
-      if (i == j) res[i][j] = 1;
+      if (i == j) res[i][j] = -2;
       else res[i][j] = 1;
     }
   }
@@ -28,7 +33,7 @@ double** fillMatrixRandom(int dim_A1, int dim_A2){
 
   for(int i = 0; i < dim_A1; i++){
     for(int j = i; j < dim_A2; j++){
-      res[i][j] = ((double)rand() / (double)RAND_MAX);
+      res[i][j] = 2*((double)rand() / (double)RAND_MAX) - 1;
       res[j][i] = res[i][j];
     }
   }
@@ -40,6 +45,15 @@ double* fillArray(int dim){
   res = (double*)malloc(dim*sizeof(double));
   for(int i = 0; i < dim; i++){
     res[i] = 1;
+  }
+  return res;
+}
+
+double* fillArrayRandom(int dim){
+  double* res;
+  res = (double*)malloc(dim*sizeof(double));
+  for(int i = 0; i < dim; i++){
+    res[i] = 2*((double)rand() / (double)RAND_MAX) - 1;
   }
   return res;
 }
@@ -118,15 +132,16 @@ double residue(double** A, int dim_A1, int dim_A2, double* x, int dim_x, double 
 void printArray(double* x, int dim_x){
   std::cout << "vector = ";
   for(int i = 0; i < dim_x;i++){
-    std::cout << std::setw(9) << x[i];
+    std::cout << std::setw(10) << x[i];
   }
   std::cout <<  std::endl;
 }
 
 void printMatrix(double** A, int dim_A1, int dim_A2){
+  std::cout << "matrix = "<< std::endl;
   for(int i = 0; i < dim_A1; i++){
     for(int j = 0; j < dim_A2; j++){
-      std::cout << std::setw(9) << A[i][j];
+      std::cout << std::setw(10) << A[i][j];
     }
     std::cout << std::endl;
   }
@@ -135,42 +150,54 @@ void printMatrix(double** A, int dim_A1, int dim_A2){
 
 int main(){
   //initialize matrix and starting vector
-
-  int dim = 30000;
+  int dim = 1000;
   int dim_x = dim, dim_A1 = dim, dim_A2 = dim;
 
-
-  double** A = fillMatrixRandom(dim_A1, dim_A2);
-  double* x = fillArray(dim_x);
-  double* Ax = (double *)malloc(dim_x*sizeof(double));
-  matvec(A, dim_A1, dim_A2, x, dim_x, Ax, dim_x);
-
-  //printMatrix(A, dim_A1, dim_A2);
-  omp_set_num_threads(4);
-
-  double mintol = 1e-6;
+  double mintol = 1e-10;
   double lambda;
   double norm;
   double tol;
+  double time;
 
-  do {
-    lambda = dot(x, dim_x, Ax, dim_x)/dot(x, dim_x, x, dim_x);
-    tol = residue(A, dim_A1, dim_A2, x, dim_x, lambda);
-    //printArray(x, dim_x);
-    matvec(A, dim_A1, dim_A2, x, dim_x, x, dim_x);
-    normalize(x, dim_x);
-    matvec(A, dim_A1, dim_A2, x, dim_x, Ax, dim_x);
-    std::cout << "tol: " << tol << " lambda: " << lambda << std::endl;
-  } while(tol > mintol);
+  double** A = fillMatrixRandom(dim_A1, dim_A2);
+  double* x = fillArrayRandom(dim_x);
+  double* Ax = (double *)malloc(dim_x*sizeof(double));
 
-  std::cout <<"max lambda = "<< lambda << std::endl;
+  //printArray(x, dim_x);
+  //printMatrix(A, dim_A1, dim_A2);
+  std::ofstream f;
+  f.open("example.txt", std::ofstream::out);
 
-  //free memory;
+
+  int threads = 9;
+  for(int i = 1; i < threads; i++){
+    omp_set_num_threads(i);
+    f << "num of threads: " << i << std::endl;
+    for(int j = 0; j < 5; j++){
+      f << j << ": ";
+      x = fillArrayRandom(dim_x);
+      normalize(x, dim_x);
+      matvec(A, dim_A1, dim_A2, x, dim_x, Ax, dim_x);
+      time = omp_get_wtime();
+      do {
+        lambda = dot(x, dim_x, Ax, dim_x);
+        tol = residue(A, dim_A1, dim_A2, x, dim_x, lambda);
+        matvec(A, dim_A1, dim_A2, x, dim_x, x, dim_x);
+        normalize(x, dim_x);
+        matvec(A, dim_A1, dim_A2, x, dim_x, Ax, dim_x);
+        //std::cout << "tol: " << tol << " lambda: " << lambda << std::endl;
+      } while(tol > mintol);
+      time = omp_get_wtime() - time;
+      f << "time: " << time << std::endl;
+      //std::cout <<"elapsed time: " << time << " with: " << i << " threads"<< std::endl;
+      free(x);
+    }
+  }
+  f.close();
   for(int i = 0; i < dim_A1; i++){
     free(A[i]);
   }
   free(A);
-  free(x);
   free(Ax);
   return 0;
 }
